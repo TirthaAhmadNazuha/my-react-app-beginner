88ca78f1e027a6a18bbb84655920affafd393e9f
TirthaAhmadNazuha <tirthaahmadnazuha.udah@gmail.com>
2022-07-09 22:30:58 +0700

    make navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
 import Navbar from './components/nav';
-import SearchBarHome from './components/home/searchbar';
-import ContentHome from './components/home/conten';
+
+
 const App = () => {
-	// document.querySelector('.home-page .con').style.overflowY = 'hidden'
-	// window.addEventListener('scroll', () => {
-	// 	const searchBarHome = document.querySelector('.home-page .search-bar-home').offsetTop + document.querySelector('.home-page .search-bar-home').clientHeight
-	// 	if (window.scrollY >= searchBarHome) {
-	// 		document.querySelector('.home-page .con').style.overflowY = 'auto'
-	// 		window.blur()
-	// 		document.querySelector('.home-page .con').focus()
-	// 	} else {
-	// 		document.querySelector('.home-page .con').style.overflowY = 'hidden'
-	// 		document.querySelector('.home-page .con').blur()
-	// 		window.focus()
-	// 	}
-	// })
+
 	return (
 		<div className='App'>
-			<div className='home-page'>
-				<SearchBarHome />
-				<ContentHome />
-			</div>
+			<h1>Hello World!</h1>
+			<p>This is pharagraf</p>
+			<div className='primary'>Text color</div>
 			<Navbar />
 		</div>
 	);
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,34 @@
 const Navbar = () => {
-    const handleClick = (e) => {
-        for (const item of e.nativeEvent.path) {
-            if (item.className === 'navbar-btn') {
-                item.className = 'navbar-btn on'
-                for (const button of document.getElementsByClassName('navbar-btn on')) {
-                    if (button.name !== item.name) button.className = 'navbar-btn'
-                }
+    const navbarInterval = () => {
+        const inter = setInterval(() => {
+            if (document.querySelector('.navbar') !== null) {
+                clearInterval(inter)
+                const btn = document.querySelectorAll('.navbar .navbar-btn')
+                const btnBg = document.querySelector('.navbar .navbar-btn-bg')
+                const onIcon = document.querySelector('.navbar .navbar-btn.on i')
+                btnBg.style.left = onIcon.offsetLeft - 8 + 'px'
+                handleClick({ btn, btnBg, onIcon })
             }
-        }
-
+        }, 0)
+    }
+    navbarInterval()
+    const handleClick = ({ btn, btnBg, onIcon }) => {
+        btn.forEach(button => {
+            button.addEventListener('click', () => {
+                btn.forEach((button) => {
+                    button.className = 'navbar-btn'
+                })
+                button.className = 'navbar-btn on'
+            })
+        })
     }
     return (
-        <div className="navbar" onClick={(e) => handleClick(e)}>
-            <button className="navbar-btn on" name="home"><div className="icon"><i className="fi fi-rr-home"></i></div><h5>Beranda</h5></button>
-            <button className="navbar-btn" name="menu"><div className="icon"><i className="fi fi-rr-apps"></i></div><h5>Menu</h5></button>
-            <button className="navbar-btn" name="promo"><div className="icon"><i className="fi fi-rr-ticket"></i></div><h5>Promo</h5></button>
-            <button className="navbar-btn" name="user"><div className="icon"><i className="fi fi-rr-user"></i></div><h5>Saya</h5></button>
+        <div className="navbar" onClick={() => navbarInterval()}>
+            <div className="navbar-btn-bg"></div>
+            <button className="navbar-btn on" name="home"><i className="fi fi-rr-home"></i><h5>Beranda</h5></button>
+            <button className="navbar-btn" name="menu"><i className="fi fi-rr-apps"></i><h5>Menu</h5></button>
+            <button className="navbar-btn" name="promo"><i className="fi fi-rr-ticket"></i><h5>Promo</h5></button>
+            <button className="navbar-btn" name="user"><i className="fi fi-rr-user"></i><h5>Saya</h5></button>
         </div>
     )
 }
